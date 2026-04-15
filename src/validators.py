92a492f25869aab58583bbb071acd855c00a33ad
Uldis Upāns <uldis.upans@gmail.com>
2026-04-15 16:48:27 +0300

    docs: add docstrings to all validators.py functions

--- src/validators.py
+++ src/validators.py
@@ -1,4 +1,24 @@
 def is_email(text):
+    """
+	Pārbauda vai teksta virkne ir derīga e-pasta adrese
+	- satur '@' un '.'
+	- nesākas ar '@' un nebeidzas ar '@' vai '.'
+
+	Args:
+	text(str): Apstrādājamā teksta virkne - epasta adrese
+	
+	Returns:
+	bool: True/False
+	
+	Example:
+	>>> is_email('anna@inbox.lv')
+	True
+	>>> is_email('anna')
+	False
+	>>> is_email('anna@inbox.')
+	False
+	"""    
+    
     if not isinstance(text,str):    # Pārbaude, vai virkne ir teksts
         return False
     
@@ -12,6 +32,24 @@ def is_email(text):
 
 
 def is_phone_number(text):
+	"""
+	Pārbauda vai ievadītais teksts atbilst Latvijas telefona numura formātam: +371 XXXXXXXX (8 cipari)
+	
+	Args:
+	text(str): Apstrādājamā teksta virkne - telefona numurs
+	
+	Returns:
+	bool: True/False
+
+	Example:
+	>>> is_phone_number('+371 26123456')
+	True
+	>>> is_phone_number('26123456')
+	False
+	>>> is_phone_number('+371 2612345')
+	False
+	"""
+         
     if not isinstance(text,str):        # Pārbaude, vai virkne ir teksts
         return False
 
@@ -24,6 +62,26 @@ def is_phone_number(text):
 
 
 def is_valid_age(age):
+	"""
+	Pārbauda vai ievadītais veselais skaitlis atbilst vecums (robežās 0-150)
+	
+	Args:
+	age(int): Vecums veselos skaitļos
+		
+	Returns:
+	bool: True/False
+
+	Example:
+	>>> is_valid_age('desmit')
+	False
+	>>> is_valid_age('10g')
+	False
+	>>> is_valid_age(10)
+	True
+	>>> is_valid_age(-10)
+	False
+	"""
+         
     if not isinstance(age, int):        #Pārbaude, vai ir vesels skaitlis
         return False
     
@@ -32,6 +90,26 @@ def is_valid_age(age):
 
 
 def is_strong_password(text):
+	"""
+	Pārbauda vai ievadītais teksts ir droša parole:
+	- vismaz 8 simboli
+	- satur burtus un ciparus
+	
+	Args:
+	text(str): Apstrādājamā teksta virkne - parole
+	
+	Returns:
+	bool: True/False
+
+	Example:
+	>>> is_strong_password('1234')
+	False
+	>>> is_strong_password('12345678')
+	False
+	>>> is_strong_password('1234567a')
+	True
+	"""
+         
     if not isinstance(text, str) or len(text) < 8:    # Pārbaude, vai virkne ir teksts un vismaz 8 simbolus garš
 
     has_digit = False
@@ -51,6 +129,24 @@ def is_strong_password(text):
 
 
 def is_valid_date(text):
+	"""
+	Pārbauda vai ievadītais teksts atbilst datuma formātam YYYY-MM-DD 
+	
+	Args:
+	text(str): Apstrādājamā teksta virkne - datums
+	
+	Returns:
+	bool: True/False
+
+	Example:
+	>>> is_valid_date('2026-26-aprīlis')
+	False
+	>>> is_valid_date('26.04.2026')
+	False
+	>>> is_valid_date('2026-04-26')
+	True
+	"""
+         
     if not isinstance(text, str) or len(text) != 10:     # Pārbaude, vai virkne ir teksts un tieši 10 simbolus garš
         return False
     
